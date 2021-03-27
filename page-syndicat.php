65529f8ebb1abd6309a9c5685bb3756c568690ce
Monocursive <cursive@protonmail.ch>
2021-03-27 16:41:05 +0100

    layout de base pages syndicats

--- page-syndicat.php
+++ page-syndicat.php
@@ -3,25 +3,25 @@
 /*
  * Template Name: Page de syndicat
  * description: >-
-  Page de syndicat pour faire un sous-site
+Page de syndicat pour faire un sous-site
  */
 get_header();
 ?>
 <div id="home">
   <div>
-    <?php 
-      if(!get_query_var( 'page' )) {
+    <?php
+if (!get_query_var('page')) {
     ?>
-    <?php get_template_part( 'template-parts/syndicat/manchette' ); ?>
-    <?php 
-      }
-    ?>
-    <?php get_template_part( 'template-parts/syndicat/posts-list' ); ?>
+    <?php get_template_part('template-parts/syndicat/manchette');?>
+    <?php
+}
+?>
+    <?php get_template_part('template-parts/syndicat/posts-list');?>
 	</div>
 
-	<?php get_template_part( 'template-parts/syndicat/sidebar' ); ?>
+	<?php get_template_part('template-parts/syndicat/sidebar');?>
 
 
 
 
-<?php get_footer(); ?>
+<?php get_footer();?>
--- template-parts/syndicat/sidebar.php
+++ template-parts/syndicat/sidebar.php
@@ -1,62 +1,58 @@
 <div id="sidebar">
 
 
-  <div id="sidebar-contact">
-		<button><a href="/contact">Nous contacter / Se syndiquer</a></button>
-  </div>
 
-	<?php wp_nav_menu( array('menu' => $pagename) ) ?>
-
+	<?php wp_nav_menu(array('menu' => $pagename, 'menu_class' => 'syndicat-contact'))?>
 	<hr />
   <div id="sidebar-campagne">
 		<h5>CAMPAGNE</h5>
 		<?php
-			$args_campagne       = array(
-				'category_name'       => 'campagne',
-				'orderby'        => 'post_date',
-				'order'          => 'DESC',
-			);
-			$the_query = new WP_Query( $args_campagne );
-		if ( $the_query->have_posts() ) :
-			?>
+$args_campagne = array(
+    'category_name' => 'campagne',
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+);
+$the_query = new WP_Query($args_campagne);
+if ($the_query->have_posts()):
+?>
 		<div id="sidebar-campagne-list">
 			<?php
-			while ( $the_query->have_posts() ) :
-				$the_query->the_post();
-				?>
-			<div>
-				<?php 
-					$value = get_field( "vignette" );
-					if($value) {
-					?>
-								<a href="<?php the_permalink(); ?>">
-						<?php
-						echo('<img src="'. $value["url"].'" />');
-						?>
-						<a>
-					<?php
-					}		
-				?>
-				</a>
+while ($the_query->have_posts()):
+    $the_query->the_post();
+    ?>
+											<div>
+												<?php
+    $value = get_field("vignette");
+    if ($value) {
+        ?>
+																<a href="<?php the_permalink();?>">
+														<?php
+    echo ('<img src="' . $value["url"] . '" />');
+        ?>
+														<a>
+													<?php
+    }
+    ?>
+												</a>
+												</div>
+											<?php endwhile;?>
 				</div>
-			<?php endwhile; ?>
-				</div>
-			<?php wp_reset_postdata(); ?>
+			<?php wp_reset_postdata();?>
 
-		<?php else : ?>
-			<p><?php _e( 'Aucun contenu disponible' ); ?></p>
-		<?php endif; ?>
+		<?php else: ?>
+			<p><?php _e('Aucun contenu disponible');?></p>
+		<?php endif;?>
   </div>
 
 		<div class="syndicats-sidebar">
 	  <h5>NOS SYNDICATS</h5>
 
 		<?php
-			wp_nav_menu( array( 
-					'theme_location' => 'syndicats-menu', 
-					'container_class' => 'syndicats-sidebar-list' ) ); 
-			?>
-		
+wp_nav_menu(array(
+    'theme_location' => 'syndicats-menu',
+    'container_class' => 'syndicats-sidebar-list'));
+?>
+
 	</div>
   </div>
 </div>
