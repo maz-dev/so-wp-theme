7101adb91a019a19f25f305d7fb44304b5d3ff52
Monocursive <cursive@protonmail.ch>
2021-03-27 14:16:37 +0100

    fix header et base page syndicat

---
+++ page-syndicat.php
@@ -0,0 +1,27 @@
+
+<?php
+/*
+ * Template Name: Page de syndicat
+ * description: >-
+  Page de syndicat pour faire un sous-site
+ */
+get_header();
+?>
+<div id="home">
+  <div>
+    <?php 
+      if(!get_query_var( 'page' )) {
+    ?>
+    <?php get_template_part( 'template-parts/syndicat/manchette' ); ?>
+    <?php 
+      }
+    ?>
+    <?php get_template_part( 'template-parts/syndicat/posts-list' ); ?>
+	</div>
+
+	<?php get_template_part( 'template-parts/syndicat/sidebar' ); ?>
+
+
+
+
+<?php get_footer(); ?>
